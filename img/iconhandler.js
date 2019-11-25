19eb37f30049072fce5857a663dcf7210b0a9fe3
Engelbert Niehaus <niehaus@uni-landau.de>
2019-11-25 18:11:39 +0100

    black images with color fill attribute added.

--- img/iconhandler.js
+++ img/iconhandler.js
@@ -1,12 +1,88 @@
 
-function handle_src(psrc) {
+function replaceString(pString,pSearch,pReplace)
+// replaces in the string "pString" multiple substrings "pSearch" by "pReplace"
+{
+	var vReturnString = '';
+	//alert("cstring.js - replaceString() "+pString);
+	pString = pString || "undefined string parameter in replaceString() call ";
+	if (!pString) {
+		console.log("replaceString()-Call - pString not defined!");
+	} else if (pString != '') {
+		var vHelpString = '';
+    var vN = pString.indexOf(pSearch);
+		while ( vN+1 > 0 ) {
+			if (vN > 0) {
+				vReturnString += pString.substring(0, vN);
+			};
+			vReturnString += pReplace;
+            if (vN + pSearch.length < pString.length) {
+				pString = pString.substring(vN+pSearch.length, pString.length);
+			} else {
+				pString = ''
+			}
+			vN = pString.indexOf(pSearch);
+		};
+	};
+	return (vReturnString + pString);
+}
+
+function capitalizeFirstLetter(string) {
+		if (!string) {
+			string = "undef_string"
+		};
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+function correct_size_100_percent(pData) {
+  if (pData) {
+    pData = replaceString(pData,'width="14px"','width="100%"');
+    pData = replaceString(pData,'height="14px"','height="100%"');
+  } else {
+    pData = "undefined image";
+  }
+  return pData;
+}
+
+
+function handle_src(picon,piconcolor) {
+  console.log("CALL: handle_src('" + picon.name + "')");
   var src = "";
-  if (psrc) {
-    src = psrc;
-    // prefix: data:image/svg+xml;
-    var h = get_data_url_header(src);
-    var s = remove_data_url_header(src);
-    var a = as
+  var a = "";
+  if (picon) {
+    if (picon.hasOwnProperty("src")) {
+      console.log("CALL: handle_src() - picon.src is defined ");
+      src = picon.src;
+      a   = picon.src;
+      // prefix: data:image/svg+xml;
+      // format: base64,
+      var h = get_data_url_header(src);
+      var s = remove_data_url_header(src);
+      if (piconcolor) {
+        //a = btoa(s);
+        a = atob(s);
+        console.log("CALL: piconcolor defined piconcolor='" + piconcolor + "'");
+        if (picon.name.indexOf("-black.svg") > 0) {
+          console.log("ICON: '" + picon.name + "' has black default color");
+          a = replaceString(a,'style="fill:#000"','style="fill:' + piconcolor + '"');
+          a = replaceString(a,'fill="#000"','fill="#FFF"');
+        } else if (picon.name.indexOf("-white.svg") > 0) {
+          console.log("ICON: '" + picon.name + "' has white default color");
+          a = replaceString(a,'style="fill:currentColor"','style="fill:' + piconcolor + '"');
+          a = replaceString(a,'fill="currentColor"','fill="#000"');
+        } else {
+          console.log("ICON: '" + picon.name + "' set with color '" + piconcolor + "'");
+          a = replaceString(a,'style="fill:currentColor"','style="fill:' + piconcolor + '"');
+          a = replaceString(a,'fill="currentColor"','fill="' + piconcolor + '"');
+        }
+        a = correct_size_100_percent(a);
+        console.log("SVG: '" + picon.name + "' " + a);
+        src = h + "," + btoa(a);
+      } else {
+        console.log("ICON COLOR: color of icon is undefined");
+      }
+    }
+
     //src = src.replace(/\n/g,"");
     //src = src.replace(/\\n/g," ");
     //src = src.replace(/\\n/g,"\n");
@@ -14,7 +90,7 @@ function handle_src(psrc) {
     //src = src.replace(/standalone="no"/g,"");
     //src = src.replace(/"/g,"'");
   };
-  console.log("SVG: "+src);
+  console.log("SVG: "+a);
   return src;
 }
 
@@ -44,6 +120,7 @@ function remove_data_url_header(psrc) {
 
 
 function icon2node(pnode,pname,pext,piconcolor) {
+  var iconcolor = piconcolor || "#ff0000";
   console.log("CALL: icon2node(pnode,pname,pext)");
   if (pnode) {
     var i4m = vDataJSON.json4icons.icons || [];
@@ -64,8 +141,7 @@ function icon2node(pnode,pname,pext,piconcolor) {
     }
     if (found >= 0) {
       console.log("Found the image-tag for '" + pname + "." + pext + "'");
-      pnode.setAttribute("src",handle_src(i4m[found].src,piconcolor));
-
+      pnode.setAttribute("src",handle_src(i4m[found],iconcolor));
       console.log("src-attribute defined for '" + pname + "." + pext + "'")
     } else {
       console.error("ERROR: icon not found in Icons4Menu");

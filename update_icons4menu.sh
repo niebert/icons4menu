f1c8357404c89a7b8c3fe7c4bedb4ee631e0e65f
Engelbert Niehaus <niehaus@uni-landau.de>
2020-07-27 13:25:41 +0200

    Bugfix of package - update_wget_icons.sh updated so that it downloads the to the current folder

--- update_icons4menu.sh
+++ update_icons4menu.sh
@@ -3,7 +3,7 @@ echo "------------------------------------------"
 echo "This file 'update_wget_icons.sh' is usually stored in the '/img' folder!"
 echo "This script downloads: https://niebert.github.io/icons4menu/wget_icons.sh"
 echo "and stores the script in the parent directory. Check the script if it does the expected job for you!"
-wget https://niebert.github.io/icons4menu/wget_icons.sh -O ../wget_icons.sh
-echo 
-cd ..
-sh ./wget_icons.sh svg
+wget https://niebert.github.io/icons4menu/wget_icons.sh -O wget_icons.sh
+# echo
+# cd ..
+# sh ./wget_icons.sh svg
